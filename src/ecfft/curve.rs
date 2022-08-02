851625b0d628fcd73b1e8e11bf9a94798c56ff9f
ashWhiteHat <phantomofrotten@gmail.com>
2022-08-02 19:04:24 +0900

    define subgroup generator and representative

--- src/ecfft/curve.rs
+++ src/ecfft/curve.rs
@@ -1,8 +1,16 @@
+//! This curve is used for generating ecfft params
+//!
+//! y^2 = x^3 + x + 0x34524f71a21a7096c6ad51a6a7fe7a43d76d8e4277cb9048edc87ab655e55142
+//! over past curve Fp p = 0x40000000000000000000000000000000224698fc094cf91b992d30ed00000001
+//! a = 1
+//! b = 23665697887148517506426806798051226694671519983424102823343279587811911881026
+//! n = 2^12 | #E(Fp)
+
 use core::fmt::{self, Debug};
 
 use ff::Field;
 use pasta_curves::Fp;
-use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
+use subtle::{Choice, ConstantTimeEq};
 
 #[derive(Clone)]
 pub(crate) struct EpAffine {
@@ -37,6 +45,40 @@ impl EpAffine {
                 0xedd674d51f009506,
                 0x17abe167e59849de,
                 0x620e16d2e51fdfa,
+            ]),
+        }
+    }
+
+    fn subgroup_generator() -> Self {
+        Self {
+            x: Fp::from_raw([
+                0x4da26202dffa62a8,
+                0x8406aac002f8b832,
+                0xf60aecbfc30e57f7,
+                0x1d62ba1b544c4f84,
+            ]),
+            y: Fp::from_raw([
+                0x52f8ada18c2b96dc,
+                0xedd674d51f009506,
+                0x17abe167e59849de,
+                0x620e16d2e51fdfa,
+            ]),
+        }
+    }
+
+    fn representative() -> Self {
+        Self {
+            x: Fp::from_raw([
+                0x5b1575e3d64364c7,
+                0x8f4b66263e159043,
+                0xb8787be360f5270b,
+                0x1ac2d405d53f2333,
+            ]),
+            y: Fp::from_raw([
+                0x641bc6c587cf1cb9,
+                0x6082deb9cca88fef,
+                0x802db2846bbce985,
+                0x3488bbb09671330,
             ]),
         }
     }
@@ -102,6 +144,42 @@ impl Ep {
         }
     }
 
+    fn subgroup_generator() -> Self {
+        Self {
+            x: Fp::from_raw([
+                0x4da26202dffa62a8,
+                0x8406aac002f8b832,
+                0xf60aecbfc30e57f7,
+                0x1d62ba1b544c4f84,
+            ]),
+            y: Fp::from_raw([
+                0x52f8ada18c2b96dc,
+                0xedd674d51f009506,
+                0x17abe167e59849de,
+                0x620e16d2e51fdfa,
+            ]),
+            z: Fp::from_raw([1, 0, 0, 0]),
+        }
+    }
+
+    fn representative() -> Self {
+        Self {
+            x: Fp::from_raw([
+                0x5b1575e3d64364c7,
+                0x8f4b66263e159043,
+                0xb8787be360f5270b,
+                0x1ac2d405d53f2333,
+            ]),
+            y: Fp::from_raw([
+                0x641bc6c587cf1cb9,
+                0x6082deb9cca88fef,
+                0x802db2846bbce985,
+                0x3488bbb09671330,
+            ]),
+            z: Fp::from_raw([1, 0, 0, 0]),
+        }
+    }
+
     fn identity() -> Self {
         Self {
             x: Fp::zero(),
@@ -132,11 +210,23 @@ mod tests {
     use super::{Ep, EpAffine};
 
     #[test]
-    fn test_is_on_curve() {
+    fn test_const_points_is_on_curve() {
         let affine_generator = EpAffine::generator();
         let projective_generator = Ep::generator();
 
+        let affine_subgroup_generator = EpAffine::subgroup_generator();
+        let projective_subgroup_generator = Ep::subgroup_generator();
+
+        let affine_representative = EpAffine::representative();
+        let projective_representative = Ep::representative();
+
         assert_eq!(affine_generator.is_on_curve().unwrap_u8(), 1);
         assert_eq!(projective_generator.is_on_curve().unwrap_u8(), 1);
+
+        assert_eq!(affine_subgroup_generator.is_on_curve().unwrap_u8(), 1);
+        assert_eq!(projective_subgroup_generator.is_on_curve().unwrap_u8(), 1);
+
+        assert_eq!(affine_representative.is_on_curve().unwrap_u8(), 1);
+        assert_eq!(projective_representative.is_on_curve().unwrap_u8(), 1);
     }
 }
