72c746351ead6d806d29374376982328a36e974a
ashWhiteHat <phantomofrotten@gmail.com>
2022-08-15 14:31:14 +0900

    modify default domain

--- src/ecfft/utils.rs
+++ src/ecfft/utils.rs
@@ -14,6 +14,7 @@ impl EcFftParams {
         let n = 1 << k;
         let acc = Ep::generator();
         let presentative = Ep::representative();
+        let isogeny = Isogeny::new(0);
 
         let mut s = Vec::new();
         let mut s_prime = Vec::new();
@@ -21,9 +22,9 @@ impl EcFftParams {
         (0..n).for_each(|i| {
             let coset = presentative + acc * Fp::from_raw([i, 0, 0, 0]);
             if i % 2 == 0 {
-                s.push(coset.to_affine().point_projective())
+                s.push(isogeny.evaluate(coset.to_affine().point_projective()))
             } else {
-                s_prime.push(coset.to_affine().point_projective())
+                s_prime.push(isogeny.evaluate(coset.to_affine().point_projective()))
             }
         });
 
@@ -56,12 +57,6 @@ mod tests {
         let k = 14;
         let ecfft_params = EcFftParams::new(k);
         let (mut s, mut s_prime) = ecfft_params.get_domain();
-        let isogeny = Isogeny::new(0);
-        s = s.iter().map(|coeff| isogeny.evaluate(*coeff)).collect();
-        s_prime = s_prime
-            .iter()
-            .map(|coeff| isogeny.evaluate(*coeff))
-            .collect();
 
         for i in 1..k {
             let isogeny = Isogeny::new(i);
